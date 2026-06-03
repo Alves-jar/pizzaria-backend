71efadd7c083d2307e35829fb03e54c695f2ad45
AlvesKZ <igoralvex01@gmail.com>
2026-06-03 20:13:37 -0300

    create order model

--- models.py
+++ models.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
 from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
+
 
 db = create_engine("sqlite:///database.db")
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = "users"
@@ -20,3 +23,26 @@ class User(Base):
         self.password = password
         self.active = active
         self.admin = admin
+        
+        
+class Order(Base):
+    __tablename__ = "orders"
+    
+    ORDER_STATUS = (
+    ("PENDING", "PENDING"),
+    ("CANCELED", "CANCELED"),
+    ("FINALIZED", "FINALIZED")
+    )
+    
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", ChoiceType(choices=ORDER_STATUS))
+    user = Column("user", ForeignKey("users.id"))
+    price = Column("price", Float)
+    # items =
+    
+    def __init__(self, user, status="PENDING", price=0):
+        self.user = user
+        self.status = status
+        self.price = price
+        
+        
