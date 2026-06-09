8813a6001cae31a041476220d2879d5d95b6d32f
AlvesKZ <igoralvex01@gmail.com>
2026-06-09 15:22:47 -0300

    create user schema

--- auth_routes.py
+++ auth_routes.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models import User, db
 from sqlalchemy.orm import sessionmaker
 from dependencies import get_session
 from main import bcrypt_context
+from schemas import UserSchema
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -12,13 +13,13 @@ async def home():
     return {"message": "You've accessed the auth route.", "authenticated": False}
 
 @auth_router.post("/create")
-async def create(name: str, email: str, password: str, session = Depends(get_session)):
-    user = session.query(User).filter(User.email==email).first()
+async def create(user_schema: UserSchema, session = Depends(get_session)):
+    user = session.query(User).filter(User.email==user_schema.email).first()
     if user:
-       return {"message": "The provided email has been registered already"} 
+       raise HTTPException(status_code=400, detail="The provided email has been registered already")
     else:
-        encrypted_password = bcrypt_context.hash(password)
-        new_user = User(name, email, encrypted_password)
+        encrypted_password = bcrypt_context.hash(user_schema.password)
+        new_user = User(user_schema.name, user_schema.email, encrypted_password, user_schema.active, user_schema.admin)
         session.add(new_user)
         session.commit()
-        return {"message": "User registered successfully"}
+        return {"message": f"User registered successfully {user_schema.email}"}
