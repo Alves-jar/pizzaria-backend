13bdde45dc9c82cab7bcf7302e799bdc25e3c771
AlvesKZ <igoralvex01@gmail.com>
2026-06-09 14:59:10 -0300

    cryptograph users password

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from models import User, db
 from sqlalchemy.orm import sessionmaker
+from dependencies import get_session
+from main import bcrypt_context
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -10,14 +12,13 @@ async def home():
     return {"message": "You've accessed the auth route.", "authenticated": False}
 
 @auth_router.post("/create")
-async def create(name: str, email: str, password: str):
-    Session = sessionmaker(bind=db)
-    session = Session()
+async def create(name: str, email: str, password: str, session = Depends(get_session)):
     user = session.query(User).filter(User.email==email).first()
     if user:
        return {"message": "The provided email has been registered already"} 
     else:
-        new_user = User(name, email, password)
+        encrypted_password = bcrypt_context.hash(password)
+        new_user = User(name, email, encrypted_password)
         session.add(new_user)
         session.commit()
         return {"message": "User registered successfully"}
